c2bcca915a3841df959fa650c950c51a036f944f
Alexakosss <alexmovsisyan1@gmail.com>
2026-07-19 00:09:30 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Έλα να σου πάρω την κούραση μωρό😘",
+        name: "Δίας Μόνο Ρε Άμανε",
         // Activity type number (0 = Playing).
         type: 0,
       },
